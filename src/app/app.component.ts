2f82160e5b4e55d5fe7981256af79c5fb230774d
Sreekanth <sreekanth.y789@gmail.com>
2021-02-23 13:34:55 +0530

    Angualr Tutorial First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>im a inline 
+  Template</h1>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
